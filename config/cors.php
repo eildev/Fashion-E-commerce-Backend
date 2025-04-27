6b8451229f187b5b03b0787f99cbe36c9c29f18f
omer4ruq <omerfaruq4@gmail.com>
2025-04-27 12:32:24 +0600

    has issue to fix

--- config/cors.php
+++ config/cors.php
@@ -2,11 +2,12 @@
 
 return [
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'register', 'logout'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    // 'allowed_origins' => ['*'],
+    'allowed_origins' => ['http://localhost:3000', 'http://127.0.0.1:3000', 'http://127.0.0.1:8000'],
 
     'allowed_origins_patterns' => [],
 
